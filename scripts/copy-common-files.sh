802333b7cddb651a9e9b51271bf4f0207f608faf
Naaman Curtis <ncurtis@kidsloop.live>
2022-04-29 01:09:16 +0100

    feat(templates): add a common setting file that is copied across all regions

---
+++ scripts/copy-common-files.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+find ./config/settings -type d -exec cp ./config/settings/common.json {} \;
--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 export FC_SETTINGS=/etc/krakend/config/settings/$ENVIRONMENT/$REGION
+cp /etc/krakend/config/settings/common.json ${FC_SETTINGS}/common.json
 
 /usr/bin/krakend check -c /etc/krakend/krakend.json
